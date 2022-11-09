578d9e28950f11bed49aaa15e616406290990776
Lucas LeBlanc <lucas.leblanc@mths.ca>
2022-11-09 21:14:42 +0000

    added file

--- weekday.cpp
+++ weekday.cpp
@@ -1,54 +1,46 @@
 // Copyright(c) 2022 Lucas LeBlanc All rights reserved.
 //
-// Created by: Lucas LeBlanc
-// Created on: October 2022
-// This program tells you what day of the weak it is
+// Created by : Lucas LeBlanc
+// Created on : Oct 2022
+// ICS3U-Unit3-05.cpp File, learning select cases in C++.
 
 #include <iostream>
 
-using namespace std;
-
 int main() {
+    // creating variables
     int weekday;
 
-    cout << "Enter weekday day number (1-7) : ";
-    cin >> weekday;
+    // input
+    std::cout << "Enter week day number 1-7: ";
+    std::cin >> weekday;
 
-    if (weekday == 1) {
-        cout << endl
-             << " The 1st day of the week is Sunday";
+    // process and output
+    std::cout << "\n";
+    switch (weekday) {
+        case 1:
+            std::cout << "The " << weekday << "st day of the week is Sunday.";
+            break;
+        case 2:
+            std::cout << "The " << weekday << "nd day of the week is Monday.";
+            break;
+        case 3:
+            std::cout << "The " << weekday << "rd day of the week is Tuesday.";
+            break;
+        case 4:
+            std::cout << "The " << weekday << "th day of the week is Wednesday.";
+            break;
+        case 5:
+            std::cout << "The " << weekday << "th day of the week is Thursday.";
+            break;
+        case 6:
+            std::cout << "The " << weekday << "th day of the week is Friday.";
+            break;
+        case 7:
+            std::cout << "The " << weekday << "th day of the week is Saturday.";
 
-    } else if (weekday == 2) {
-        cout << endl
-             << "The 2nd day of the week is Monday";
-
-    } else if (weekday == 3) {
-        cout << endl
-             << "The 3rd day of the week is Tuesday";
-
-    } else if (weekday == 4) {
-        cout << endl
-             << "The 4th day of the week is Wednesday";
-
-    } else if (weekday == 5) {
-        cout << endl
-             << "The 5th day of the week is Thursday";
-
-    } else if (weekday == 6) {
-        cout << endl
-             << "The 6th day of the week is Friday";
-
-    } else if (weekday == 7) {
-        cout << endl
-             << "The 7th day of the week is Saturday";
-
-    } else {
-        cout << endl
-             << "This is not a number of the week.";
+        default:
+            std::cout << "This is not a number of the week.";
     }
 
-    return 0;
-
-    std::cout << std::endl;
-    std::cout << "Done.";
+    std::cout << "\n\n\nDone.\n";
 }
